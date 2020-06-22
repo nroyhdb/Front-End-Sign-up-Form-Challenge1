3835ab2da062c6e107e27344d5108b7f1f8b11b0
Nibedita Roy <67225276+nroyhdb@users.noreply.github.com>
2020-06-23 02:25:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', e=>{
+    e.preventDefault();
+    const firstName=form['firstname'].value;
+    const lastName=form['lastname'].value;
+    const email=form['email'].value;
+    const password=form['password'].value;
+
+    if(firstName === ''){
+        addErrorTo('firstname','First Name cannot be empty');
+    } else{
+        removeErrorFrom('firstname');
+    }
+   
+    if(lastName === ''){
+        addErrorTo('lastname','Last Name cannot be empty');
+    } else{
+        removeErrorFrom('lastname');
+    }
+
+    if(email === ''){
+        addErrorTo('email','Email is required');
+    } else if(isValid(email)){
+        addErrorTo('email','looks like this is not an email');
+    } else{
+        removeErrorFrom('email');
+    }
+
+    if(password === ''){
+        addErrorTo('password','Password cannot be empty');
+    } else{
+        removeErrorFrom('password');
+    }
+
+});
+
+function addErrorTo(field, message){
+    const formControl = form[field].parentNode;
+    formControl.classList.add('error');
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+    small.style.opacity='1';
+}
+
+function removeErrorFrom(field){
+    const formControl = form[field].parentNode;
+    formControl.classList.add('error');
+    const small = formControl.querySelector('small');
+    small.style.opacity= '0';
+}
+function isValid(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
